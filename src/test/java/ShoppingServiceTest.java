91c4486ed8f66ff82b9845ffdfebdb6e4aa915bf
Phil-Shepard <Philipp-74@yandex.ru>
2023-12-12 17:45:43 +0500

    Написал тесты

--- src/test/java/ShoppingServiceTest.java
+++ src/test/java/ShoppingServiceTest.java
@@ -1,35 +1,112 @@
 import customer.Customer;
-import customer.CustomerDao;
 import customer.CustomerService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import product.Product;
+import product.ProductDao;
+import shopping.BuyException;
 import shopping.Cart;
-import shopping.ShoppingService;
 import shopping.ShoppingServiceImpl;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.mockito.Mockito.when;
+
+/**
+ * Тестирование класса {@link ShoppingServiceImpl}
+ */
 public class ShoppingServiceTest {
-    private final CustomerDao productDaoMock = Mockito.mock(CustomerDao.class);
-    private final Cart cart = Mockito.mock(Cart.class);
-    private final ShoppingServiceImpl shoppingService =
-            new ShoppingServiceImpl(cart);
+
+    public final ProductDao productDao = Mockito.mock(ProductDao.class);
+
+    private final ShoppingServiceImpl shoppingService = new ShoppingServiceImpl(productDao);
+
+
+    /**
+     * Тест покупки продуктов
+     */
     @Test
-    public void testAddCart() throws Exception {
-        when(customerDaoMock.save(any(Customer.class)))
-                .thenReturn(Boolean.TRUE);
+    void testBuyProducts() throws BuyException {
+        Product firstProduct = new Product();
+        firstProduct.setName("eggs");
+        firstProduct.addCount(10);
 
+        Product secondProduct = new Product();
+        secondProduct.setName("sugar");
+        secondProduct.addCount(2);
 
+        Customer customer = new Customer(3L, "3");
+        Cart cart = new Cart(customer);
+        cart.add(firstProduct, 1);
+        cart.add(secondProduct, 1);
+
+        boolean result = shoppingService.buy(cart);
+        Mockito.verify(productDao, Mockito.times(1)).save(firstProduct);
+        Mockito.verify(productDao, Mockito.times(1)).save(secondProduct);
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(9, firstProduct.getCount());
+        Assertions.assertEquals(1, secondProduct.getCount());
     }
 
+
+    /**
+     * Тест метода получения всех продуктов, находящихся в корзине
+     */
     @Test
-    public void testAddCart() throws Exception {
-        when(customerDaoMock.save(any(Customer.class)))
-                .thenReturn(Boolean.TRUE);
+    public void testGetAllProducts() {
+        Product product1 = new Product();
+        product1.setName("apples");
+        product1.addCount(10);
 
+        Product product2 = new Product();
+        product2.setName("pens");
+        product2.addCount(2);
 
+        List<Product> mockProducts = Arrays.asList(product1, product2);
+
+        when(productDao.getAll()).thenReturn(mockProducts);
+
+        List<Product> products = shoppingService.getAllProducts();
+
+        Assertions.assertEquals(mockProducts, products);
+    }
+
+    /**
+     * Тест метода получения продукта по имени
+     */
+    @Test
+    public void testGetProductByName() {
+        Product product1 = new Product();
+        product1.setName("apples");
+        product1.addCount(10);
+
+        String productName = product1.getName();
+        when(productDao.getByName(productName)).thenReturn(product1);
+
+        Product product = shoppingService.getProductByName(productName);
+
+        Assertions.assertEquals(product1, product);
+    }
+
+    /**
+     * Тест получения корзины
+     */
+    @Test
+    void testGetCart() {
+        Customer customer = new Customer(1, "1");
+        Assertions.assertNotNull(shoppingService.getCart(customer));
+    }
+
+    /**
+     * Тест покупки товаров из пустой корзины
+     */
+    @Test
+    void testBuyEmptyCart() throws BuyException {
+        Customer customer = new Customer(2L, "2");
+        Cart cart = new Cart(customer);
+        boolean result = shoppingService.buy(cart);
+        Assertions.assertFalse(result);
     }
 }
